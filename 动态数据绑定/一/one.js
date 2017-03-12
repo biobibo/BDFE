795254bdd6eadeb913fba5db4f0e27fe476da7dc
biobibo <lei840680270@gmail.com>
2017-03-12 16:14:50 +0800

    commit task

---
+++ 动态数据绑定/一/one.js
@@ -0,0 +1,40 @@
+function Observer(data) {
+  this.data=data;
+  this.walk(data);
+}
+//在原型对象上创建属性方法
+let pro = Observer.prototype;
+
+pro.walk=function (obj) {
+  let val;
+  for (let key in obj){
+    if (obj.hasOwnProperty(key)){
+        val=obj[key];
+        if (typeof val ==='object'){
+          new Observer(val);
+        }
+        this.convert(key,val);
+    }
+  }
+};
+pro.convert=function (key,val) {
+  Object.defineProperty(this.data,key,{
+    // configurable:true,
+    // writable:true,
+    // enumerable:true,
+    get:function () {
+      console.log('你访问了'+ key+'  '+'其值为：'+val);
+      return  val;
+    },
+    set:function (newVal) {
+      if (val === newVal) {return '';}
+      else{
+      console.log('你设置了'+key+'  '+'新的值为：'+newVal);
+      val=newVal;
+      }
+
+    }
+  })
+};
+
+
